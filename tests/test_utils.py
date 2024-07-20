1748494da534355433731500c953c3d8a42bda1e
jakevc <jake.vancampen7@gmail.com>
2024-07-20 12:39:38 -0700

    simplify so not redundant account name

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,30 +1,27 @@
 from snakemake_storage_plugin_azure.utils import (
-    parse_query_account_name,
+    container_name_is_valid,
     parse_query_container_name,
     parse_query_path,
 )
 
 
-def test_parse_query_account_name():
-    assert (
-        parse_query_account_name("az://acct/container/path/example/file.txt") == "acct"
-    )
-    assert (
-        parse_query_account_name("az://test/container/path/example/file.txt") == "test"
-    )
-
-
 def test_parse_query_container_name():
     assert (
-        parse_query_container_name("az://acct/container/path/example/file.txt")
+        parse_query_container_name("az://container/path/example/file.txt")
         == "container"
     )
 
 
 def test_parse_query_path():
     assert (
-        parse_query_path("az://acct/container/path/example/file.txt")
-        == "path/example/file.txt"
+        parse_query_path("az://container/path/example/file.txt")
+        == "/path/example/file.txt"
     )
-    assert parse_query_path("az://acct/container/path/file.txt") == "path/file.txt"
-    assert parse_query_path("az://acct/container/file.txt") == "file.txt"
+    assert parse_query_path("az://container/path/file.txt") == "/path/file.txt"
+    assert parse_query_path("az://container/file.txt") == "/file.txt"
+
+
+def test_container_name_is_valid():
+    assert container_name_is_valid("container")
+    assert container_name_is_valid("container-test")
+    assert not container_name_is_valid("container**notvalid")
